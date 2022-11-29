903874018c5e2ffa4f2ea1354935c8544edcd138
hkimi amin <hkimiamin02@gmail.com>
2022-11-29 15:17:00 +0100

    import bootsrap and icons and images

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
+
+createApp(App).use(store).use(router).use(bootstrap).mount('#app');

6a972b542babdd49026a42ebc20976eb910ef35f
hkimi amin <hkimiamin02@gmail.com>
2022-11-30 19:38:04 +0100

    add component

---
+++ src/components/merchArticle.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="col">
+    <div class="product_card">
+        <div class="imgBx">
+            <img src="../assets/images/banner.jpg" alt="">
+        </div>
+        <div class="contextBx">
+            <h3>Wireless Headphone</h3>
+            <h2 class="price">$245<small>.22</small></h2>
+            <a href="#" class="buy">Buy now</a>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default{
+    name:'mecrhArticle',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style scoped>
+.product_card {
+    position: relative;
+    width: 320px;
+    height: 420px;
+    background: #122936;
+    border-radius: 20px;
+    overflow: hidden;
+}
+
+.product_card::before {
+    content: '';
+    position: absolute;
+    top: -50%;
+    width: 100%;
+    height: 100%;
+    background: #FFBF00;
+    transform: skewY(345deg);
+    transition: 0.5s;
+}
+
+.product_card:hover::before {
+    top: -60%;
+    transform: skewY(390deg);
+}
+
+.product_card::after {
+    content: 'Urban';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    font-weight: 600;
+    font-size: 8em;
+    color: rgba(0, 0, 0, 0.1);
+}
+
+.imgBx {
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    z-index: 1;
+}
+
+.imgBx img {
+    max-width: 90%;
+    transition: 0.5s;
+}
+
+.product_card:hover .imgBx img {
+    max-width: 70%;
+}
+
+.contextBx {
+    position: relative;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    z-index: 1;
+}
+
+.contextBx h3 {
+    font-size: 18px;
+    color: #fff;
+    font-weight: 500;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+}
+
+.contextBx .price {
+    font-size: 24px;
+    color: #fff;
+    font-weight: 500;
+    letter-spacing: 1px;
+}
+
+.contextBx .buy {
+    position: relative;
+    top: 200px;
+    opacity: 0;
+    padding: 10px 30px;
+    margin-top: 15px;
+    color: #fff;
+    text-decoration: none;
+    background: #FFBF00;
+    border-radius: 30px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    transition: 0.5s;
+}
+
+.contextBx .buy {
+    top: 0;
+    opacity: 1;
+}
+</style>
--- src/views/StoreView.vue
+++ src/views/StoreView.vue
@@ -1,3 +1,35 @@
 <template>
     
+    <div class="row store">
+        <h2 class="row title text-center">welcome to the official mouvma Store all mouvma merch in one place</h2>
+    <mecrhArticle></mecrhArticle>
+</div>
 </template>
+
+<script>
+// @ is an alias to /src
+import mecrhArticle from '@/components/merchArticle.vue'
+
+export default {
+  name: 'StoreView',
+  components: {
+    mecrhArticle
+  },
+  data(){
+    return{
+        products:[
+            {
+
+            }
+        ]
+    }
+  }
+}
+</script>
+<style>
+.store{
+    margin-top: 5%;
+    margin-bottom: 5%;
+    margin-left: 3%;
+}
+</style>
